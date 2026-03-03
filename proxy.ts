deee5bda310e17e53966898affd85617091b8281
Ardiansyah <ardiiasyh@gmail.com>
2026-03-03 14:57:12 +0800

    fix proxy export name

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const uid = req.cookies.get("uid")?.value
   const role = req.cookies.get("role")?.value
   const status = req.cookies.get("status")?.value
